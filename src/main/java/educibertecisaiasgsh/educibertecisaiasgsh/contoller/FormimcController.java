ab8fb32c33fc5684eb867de51d2efcb932668a74
goshaaaa777 <isaiasddx322z@gmail.com>
2023-09-17 17:01:27 -0500

    desarrollo de ejercicio

--- src/main/java/educibertecisaiasgsh/educibertecisaiasgsh/contoller/FormimcController.java
+++ src/main/java/educibertecisaiasgsh/educibertecisaiasgsh/contoller/FormimcController.java
@@ -1,4 +1,42 @@
 package educibertecisaiasgsh.educibertecisaiasgsh.contoller;
 
+import educibertecisaiasgsh.educibertecisaiasgsh.model.PromedioModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class FormimcController {
+
+    @GetMapping("/promedionotas")
+    public String index(Model model){
+        model.addAttribute("promediomodel",
+                new PromedioModel());
+        model.addAttribute("verresultado",
+                false);
+            return "formpromedio";
+        }
+        @PostMapping("/promedionotas")
+    public String calcularpromedio(
+
+            @ModelAttribute("promediomodel")
+                PromedioModel promedioModel,
+            Model model
+            ){
+        Double promedio = (promedioModel.getCl1()*0.15)+
+                (promedioModel.getCl2()*0.2)+
+                (promedioModel.getCl3()*0.35)+
+                (promedioModel.getProyectF()*0.3);
+        String condicion = (promedio > 10.5) ? "Aprobado" : "Desaprobado";
+
+        model.addAttribute("resultado",
+            "El promedio de notas es: " + promedio + " usted se encuentra " + condicion);
+        model.addAttribute("verresultado", true);
+        model.addAttribute("promediomodel", new PromedioModel());
+
+        return "formpromedio";
+    }
 }
--- src/main/java/educibertecisaiasgsh/educibertecisaiasgsh/model/PromedioModel.java
+++ src/main/java/educibertecisaiasgsh/educibertecisaiasgsh/model/PromedioModel.java
@@ -1,4 +1,17 @@
 package educibertecisaiasgsh.educibertecisaiasgsh.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PromedioModel {
+    private Integer cl1;
+    private Integer cl2;
+    private Integer cl3;
+    private Integer proyectF;
 }
