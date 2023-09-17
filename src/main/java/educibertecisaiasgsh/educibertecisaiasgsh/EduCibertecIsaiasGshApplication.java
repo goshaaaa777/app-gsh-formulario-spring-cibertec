de10c71c4ff8cbf091ed50227020bcea0fbed976
goshaaaa777 <isaiasddx322z@gmail.com>
2023-09-17 14:18:24 -0500

    inicio gsh

---
+++ src/main/java/educibertecisaiasgsh/educibertecisaiasgsh/EduCibertecIsaiasGshApplication.java
@@ -0,0 +1,13 @@
+package educibertecisaiasgsh.educibertecisaiasgsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCibertecIsaiasGshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCibertecIsaiasGshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/educibertecisaiasgsh/educibertecisaiasgsh/EduCibertecIsaiasGshApplicationTests.java
@@ -0,0 +1,13 @@
+package educibertecisaiasgsh.educibertecisaiasgsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCibertecIsaiasGshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
