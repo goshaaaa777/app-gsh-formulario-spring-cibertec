eb31b464b9f4f7ee9d4542960dfe05de0553ca6c
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2023-09-24 15:40:05 -0500

    ejercicio 2 terminado

--- src/main/java/educibertecisaiasgsh/educibertecisaiasgsh/model/ImcModel.java
+++ src/main/java/educibertecisaiasgsh/educibertecisaiasgsh/model/ImcModel.java
@@ -1,4 +1,12 @@
 package educibertecisaiasgsh.educibertecisaiasgsh.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class ImcModel {
+    private Double talla;
+    private Double peso;
 }
